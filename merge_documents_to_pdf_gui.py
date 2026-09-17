598b4274f648e163e772c46cc3d416d9a77293cb
Christopher Joseph Conti <36709920+contic5@users.noreply.github.com>
2026-09-17 10:57:57 -0400

    Updated GUI to be easier to use

--- merge_documents_to_pdf_gui.py
+++ merge_documents_to_pdf_gui.py
@@ -1,54 +1,74 @@
 import tkinter as tk
+from tkinter import ttk  # Import themed widgets
 from tkinter import filedialog
-import time
 
 from pathlib import Path
 from docx2pdf import convert
 from docx import Document
 from docxcompose.composer import Composer
+import shutil
 
-def merge_student_files(folder_location,student):
-    # Set the directory path
-    dir_path = Path(folder_location)
-    total_pdfs=0
-    total_docx=0
-    composer = Composer(Document())
+class Merge_Documents_GUI(tk.Tk):
+    def __init__(self):
+            super().__init__()  # Initialize the base tk.Tk class
+            self.geometry("500x400")
+            self.label = ttk.Label(self, text="Merge Word Documents to PDF",wraplength=300,font=("Arial",24),justify="center")
+            self.label.pack()
 
-    # Recursively iterate through all files and folders
-    for path in dir_path.rglob("*"):
-        depth = len(path.relative_to(dir_path).parts) - 1
-        if path.is_file():
-            if path.suffix==".docx":
-                print(f"Reading: {path}")
-                doc_to_append = Document(path)
-                composer.append(doc_to_append)
-                total_docx+=1
-            elif path.suffix==".pdf":
-                total_pdfs+=1
+            self.results_label=ttk.Label(self,text="Ready to merge documents",font=("Arial",18),wraplength=300,justify="center")
+            self.results_label.pack()
 
-    print(f"Total Docxs: {total_docx}")
-    print(f"Total PDFs: {total_pdfs}")
-    composer.save(f"output/{student} Documents Merged.docx")
-    print(f"Merged all Microsoft Word Documents for {student}")
-
-    convert(f"output/{student} Documents Merged.docx")
-    print("Converted Merged Document to PDF")
-
-def select_folder():
-    print("Merge Student Documents to PDF")
-    print("Select a Student Folder to Scan")
-    time.sleep(1)
+            self.current_file_label=ttk.Label(self,text="")
+            self.current_file_label.pack()
     
-    root = tk.Tk()
-    root.withdraw()  # Hides the main small root window
-    folder_location = filedialog.askdirectory(title="Select Folder")
+            self.button = ttk.Button(self, text="Select Folder", width=25, command=self.select_folder)
+            self.button.pack()
     
-    if folder_location:
-        student=folder_location.split("/")[-1]
-        print("Selected folder:", folder_location)
-        merge_student_files(folder_location,student)
-    else:
-        print("No folder selected.")
+    def select_folder(self):
+        #self.withdraw()  # Hides the main small root window
+        folder_location = filedialog.askdirectory(title="Select Folder")
+        
+        if folder_location:
+            student=folder_location.split("/")[-1]
+            print("Selected folder:", folder_location)
+            self.merge_student_files(folder_location,student)
+        else:
+            print("No folder selected.")
+
+    def merge_student_files(self,folder_location,student):
+        # Set the directory path
+        dir_path = Path(folder_location)
+        parent_path=dir_path.parent
+        total_pdfs=0
+        total_docx=0
+        composer = Composer(Document())
+
+        # Recursively iterate through all files and folders
+        for path in dir_path.rglob("*"):
+            depth = len(path.relative_to(dir_path).parts) - 1
+            if path.is_file():
+                if path.suffix==".docx":
+                    print(f"Reading: {path}")
+                    self.current_file_label.config(text=f"Reading {path}")
+                    doc_to_append = Document(path)
+                    composer.append(doc_to_append)
+                    total_docx+=1
+                elif path.suffix==".pdf":
+                    total_pdfs+=1
+
+        output_file_name=f"{student} Documents Merged"
+        composer.save(f"output/{output_file_name}.docx")
+        self.current_file_label.config(text=f"")
+
+        self.results_label.config(text=f"Merged all Microsoft Word Documents for {student}")
+        convert(f"output/{output_file_name}.docx")
+
+        self.results_label.config(text=f"Converted Merged {student} Documents to PDF")
+
+        if not "input" in parent_path:
+            shutil.copy2(f"output/{output_file_name}.docx", f"{parent_path}/{output_file_name}.docx")        
+            shutil.copy2(f"output/{output_file_name}.pdf", f"{parent_path}/{output_file_name}.pdf")        
 
 if __name__ == "__main__":
-    select_folder()
+    root=Merge_Documents_GUI()
+    root.mainloop()
